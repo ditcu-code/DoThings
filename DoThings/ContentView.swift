8378792d134e49dba91dc2279961a8c61f750a52
ditcu-code <work.adityac@gmail.com>
2022-04-10 23:47:25 +0700

    Initial Commit

---
+++ DoThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoThings
+//
+//  Created by Aditya Cahyo on 10/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoThings/DoThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoThingsApp.swift
+//  DoThings
+//
+//  Created by Aditya Cahyo on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DoThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
